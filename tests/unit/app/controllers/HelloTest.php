e6bf0961029b21d292d765a3b0279207a1da14d1
Marcos Minond <minond.marcos@gmail.com>
2014-03-18 14:40:36 -0600

    moving unit tests/unit to tests dir

---
+++ tests/unit/app/controllers/HelloTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MyApplication\Tests\Configuration;
+
+use PHPUnit_Framework_TestCase;
+use MyApplication\Controller\Hello;
+use Efficio\Http\Request;
+
+class HelloTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Hello
+     */
+    public $hello_controller;
+
+    public function setUp()
+    {
+        $this->hello_controller = new Hello;
+    }
+
+    public function testGreetActionUsesDefaultGreeting()
+    {
+        $this->hello_controller->greet();
+        $resource = $this->hello_controller->resource;
+        $greeting = $resource['greeting'];
+        $this->assertEquals('Hello', $greeting->label);
+    }
+
+    public function testGreetActionUsesDefaultName()
+    {
+        $this->hello_controller->greet();
+        $resource = $this->hello_controller->resource;
+        $greeting = $resource['greeting'];
+        $this->assertEquals('World', $greeting->noun);
+    }
+}
